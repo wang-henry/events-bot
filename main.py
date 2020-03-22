5c87b67deb0982d5ce44cc409393335f01d1dd31
Henry Wang <32380663+wang-henry@users.noreply.github.com>
2020-03-22 01:31:06 -0400

    Fixed removal bug and added json dump in list

--- main.py
+++ main.py
@@ -182,7 +182,7 @@ async def remove(ctx, year_in, month_in, day_in, index_in):
         json.dump(events, events_file)
 
     with open(f'data/dates/{server_id}.json', 'w') as dates_file:
-        json.dump({"dates": [date_lst]}, dates_file)
+        json.dump({"dates": date_lst}, dates_file)
 
     await ctx.message.channel.send(f"Successfully removed {removed_event} on {date_str}")
 
@@ -204,6 +204,12 @@ async def list(ctx):
     else:
         events = {}
         date_lst = []
+        # Write both objects to JSON files per server
+        with open(f'data/events/{server_id}.json', 'w') as events_file:
+            json.dump(events, events_file)
+
+        with open(f'data/dates/{server_id}.json', 'w') as dates_file:
+            json.dump({"dates": date_lst}, dates_file)
 
     embed = discord.Embed(title="Events", description="List of upcoming events:", color=0xeee657)
 
